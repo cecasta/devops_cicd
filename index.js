8f28590798aa56d3f9d46fc2ec75178f825b4323
CesarC <cesarcastao512@gmai.com>
2022-07-20 14:09:36 -0500

    cambio en el codigo

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 
 
 app.get("/", (req, res) => {
-    res.send("Task CICD - DevOps Crash Course 2022");
+    res.send("Task CICD - DevOps Crash Course ----- 2022");
     console.log("print in the server - tash CICD");
 });
 
